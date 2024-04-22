bdf76707b874841ee704e6c835ab72cc8315ea70
Sam Medawatta <sampath.medawatta@gmail.com>
2024-04-22 23:03:00 +1000

    add react router and link dashboard page in side menu bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
 
 import PublicHeader from './Components/Header';
 import PublicNavbar from './Components/Navbar';
 import PublicLogin from './Components/Login';
 import PublicFooter from './Components/Footer';
 import RegDashbord from './Components/RegUser/RegDashbord';
+import Dashboard from "./Components/User/Dashboard";
 import MobileNavbar from './Components/MobileNavbar';
 
 // Mock session object
@@ -17,13 +18,19 @@ function App() {
 
   return (
     <div className="App">
-      <PublicHeader/>
-      <MobileNavbar/>
-      <main class="main">
-      <PublicNavbar/>
-      <RegDashbord/>
-      </main>
-      <PublicFooter/>
+      <Router>
+        <PublicHeader />
+        <MobileNavbar />
+        <main class="main">
+          <PublicNavbar />
+          <Routes>
+            <Route exact path="/" element={<Dashboard />} />
+            <Route exact path="/home" element={<Dashboard />} />
+            <Route exact path="/dashboard" element={<RegDashbord />} />
+          </Routes>
+        </main>
+        <PublicFooter />
+      </Router>
     </div>
   );
 }
---
+++ client/src/Components/User/Dashboard.js
@@ -0,0 +1,10 @@
+
+import React, { useState, useEffect } from "react";
+function Dashboard() {
+  return (
+    <div className="box-content">
+        User Dashboard
+    </div>
+  );
+}
+export default Dashboard;
