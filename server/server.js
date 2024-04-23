2384a26a4b4aac80dbf6f0ac77a86f3e352cd6e9
Sam Medawatta <sampath.medawatta@gmail.com>
2024-04-23 22:35:26 +1000

    setup swagger

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
+const swaggerUi = require("swagger-ui-express");
 const express = require("express");
 
 const port = process.env.PORT || 3001
 
 const app = express();
-
 app.use(express.json());
 
 app.use("/faqs", require("./routes/faqsRoute"));
@@ -12,6 +12,10 @@ app.use("/inquiries", require("./routes/inquiryRoute"));
 app.use("/alerts", require("./routes/alertsRoute"));
 app.use("/user", require("./routes/userRoute"));
 app.use("/admin", require("./routes/adminRoute"));
+
+
+const swaggerDocument = require("./swagger-output.json");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, ()=>
   console.log('Server started on port '+ port)
