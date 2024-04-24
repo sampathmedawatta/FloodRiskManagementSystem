7b55623e0cc5a7fcd487670cc2bf3904553975cc
BKHChathuranga <chathurangabkh.19@itfac.mrt.ac.lk>
2024-04-24 23:27:01 +0530

    completed the forecast card

--- client/src/Components/RegUser/ForecastCard.js
+++ client/src/Components/RegUser/ForecastCard.js
@@ -1,16 +1,22 @@
 import React from "react";
 
-const ForecastCard = () => {
+const ForecastCard = ({forecast}) => {
+  const riskLevelClass = getRiskLevelClass(forecast.riskLevel);
+  const iconClass = getIconClass(forecast.riskLevel);
+  const formattedDate = new Date(forecast.date).toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+  });
   return (
     <div className="col-2">
-      <div className="card-style-1 hover-up risklevel-high">
+      <div className={`card-style-1 hover-up ${riskLevelClass}`}>
         <div className="card-info">
           <div className="card-title row">
             <div className="col">
-              <h6 className="text-start">April 26</h6>
+              <h6 className="text-start">{formattedDate}</h6>
             </div>
             <div className="col-auto">
-              <i className="bi bi-cloud-lightning-rain-fill fs-3 color-brand-1" />
+              <i className={`bi ${iconClass} fs-3 color-brand-1`}  />
             </div>
           </div>
           <div className="ptb-10">
@@ -46,6 +52,31 @@ const ForecastCard = () => {
       </div>
     </div>
   );
+  
 };
 
+function getRiskLevelClass(riskLevel) {
+  switch (riskLevel.toLowerCase()) {
+    case "low":
+      return "risklevel-norisk";
+    case "moderate":
+      return "risklevel-moderate";
+    case "high":
+      return "risklevel-high";
+    default:
+      return "";
+  }
+};
+function getIconClass(riskLevel) {
+  switch (riskLevel.toLowerCase()) {
+    case "low":
+      return "bi-cloud-sun-fill"; 
+    case "moderate":
+      return "bi-cloud-rain-fill"; 
+    case "high":
+      return "bi-cloud-lightning-rain-fill"; 
+    default:
+      return "";
+  }
+}
 export default ForecastCard;
