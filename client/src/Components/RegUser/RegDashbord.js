1a74a1f024182af57640328d9cedca2e1739cb55
BKHChathuranga <umatissera@gmail.com>
2024-04-25 22:23:49 +0530

    registred-user-dashboard

--- client/src/Components/RegUser/RegDashbord.js
+++ client/src/Components/RegUser/RegDashbord.js
@@ -2,17 +2,40 @@ import React, { useState, useEffect } from "react";
 
 import RegMainAlert from "./RegMainAlert";
 import RegFloodForecast5Days from "./RegFloodForecast5Days";
-import { mainAlertList } from "../../utils/MainAlerts";
+import AlertService from "../../services/alert.service";
+import { useLocation } from "../../contexts/LocationContext";
 
 function RegDashbord() {
+
+  const [alertData, setAlertData] = useState(null);
+  // const {location} = useLocation(); can use this after modifying alert data to existing locations
+  const location = "location"
+
+    useEffect(() => {
+      const fetchLocationData = async () => {
+        try {
+          const alerts = await AlertService.getAllAlerts();
+          if (alerts) {
+            // Filter alerts based on location
+            const filteredAlerts = alerts.filter((alert) => alert.location === location);
+            setAlertData(filteredAlerts);
+          }
+        } catch (error) {
+          console.error("Error fetching location data:", error);
+        }
+      };
+  
+      fetchLocationData();
+    }, []);
+  
   return (
     <div className="box-content">
-      {mainAlertList.length > 0 &&
-        mainAlertList.map((alert) => (
+      {alertData?.length > 0 &&
+        alertData?.map((alert) => (
           <RegMainAlert
             key={alert.id}
-            message={alert.message}
-            alertType={alert.type}
+            message={alert.description}
+            alertType={alert.riskLevel}
           />
         ))}
       <RegFloodForecast5Days />
---
+++ client/src/services/alert.service.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:3000/alerts",
+  withCredentials: false, // Allow cookies to be sent with requests (if applicable)
+});
+
+const headers = {
+  "Content-Type": "application/json",
+  //Authorization: "Bearer your-access-token", // Add any custom headers as needed
+};
+
+const getAlertById = async (id) => {
+  return await axiosInstance.get(`/${id}`, headers).then((response) => {
+    return response.data;
+  });
+};
+
+const getAllAlerts = async () => {
+  return await axiosInstance.get("/", { headers }).then((response) => {
+    return response.data;
+  });
+};
+
+const AlertService = {
+  getAlertById,
+  getAllAlerts,
+};
+
+export default AlertService;
