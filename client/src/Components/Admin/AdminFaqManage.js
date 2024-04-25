7b28c577bed7c8c607a579ec2365538f1b23fa2b
“Chankami” <chankami91@gmail.com>
2024-04-26 01:01:54 +1000

    Faq-admin-view

--- client/src/Components/Admin/AdminFaqManage.js
+++ client/src/Components/Admin/AdminFaqManage.js
@@ -1,6 +1,42 @@
 import React, { useState, useEffect } from "react";
+import { API_BASE_URL } from "../Shared/apiConfig";
+import axios from "axios";
 
-function AdminTodayForecast() {
+function AdminFaqManage() {
+  const [faqs, setFaqs] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/faqs/`);
+      const sortedFaqs = response.data.sort((a, b) => {
+        return new Date(a.createdTime) - new Date(b.createdTime);
+      });
+      setFaqs(sortedFaqs);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  const publishFaq = async (id, active) => {
+    try {
+      await axios.put(`${API_BASE_URL}/faqs/${id}`, { active: !active });
+      fetchData(); // Refresh data after update
+    } catch (error) {
+      console.error("Error updating status:", error);
+    }
+  };
+
+  const unpublishFaq = async (id) => {
+    try {
+      await axios.put(`${API_BASE_URL}/faqs/${id}`, { active: false });
+      fetchData(); // Refresh data after update
+    } catch (error) {
+      console.error("Error updating status:", error);
+    }
+  };
   return (
     <div className="col-md-12">
       <div className="box-content">
@@ -28,111 +64,126 @@ function AdminTodayForecast() {
                         <th
                           scope="col"
                           className="pl-4"
-                          style={{ width: "30%" }}
+                          style={{ width: "30%", textAlign: "left" }}
                         >
                           Title
                         </th>
-                        <th scope="col" style={{ width: "50%" }}>
+                        <th
+                          scope="col"
+                          style={{ width: "50%", textAlign: "left" }}
+                        >
                           Description
                         </th>
-                        <th scope="col" style={{ width: "10%" }}>
+                        <th
+                          scope="col"
+                          style={{ width: "10%", textAlign: "left" }}
+                        >
                           Status
                         </th>
-                        <th scope="col" style={{ width: "10%" }}>
+                        <th
+                          scope="col"
+                          style={{ width: "10%", textAlign: "left" }}
+                        >
                           Manage
                         </th>
                       </tr>
                     </thead>
                     <tbody>
-                      <tr>
-                        <td className="text-left pl-4">
-                          <span className="text-muted font-sm">
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing
-                            elit. Aenean
-                          </span>
-                        </td>
-                        <td className="text-left">
-                          <p className="text-muted font-sm word-limit">
-                            commodo ligula eget dolor. Aenean commodo ligula
-                            eget dolor. Aenean commodo ligula eget dolor. Aenean
-                            massa. Cum sociis natcommodo ligula eget dolor.
-                            Aenean massa. Cum sociis natcommodo ligula eget
-                            dolor. Aenean massa. Cum sociis natcommodo ligula
-                            eget dolor. Aenean massa. Cum sociis natoque
-                            penatibus et magnis dis parturient montes, nascetur
-                            ridiculus mus. Donec quam felis, ultricies nec,
-                            pellentesque eu, pretium quis, sem. Nulla consequat
-                            massa quis enim. Donec pede justo, fringilla vel,
-                            aliquet nec, vulputate eget, arcu.{" "}
-                            <span class="more-content"> .......</span>
-                          </p>
-                        </td>
-                        <td className="text-center">
-                          <span class="label-status label-inactive">
-                            Inactive
-                          </span>
-                    
-                        </td>
-                        <td className="text-center">
-                          <button type="button" class="btn btn-pops">
-                            <i class="bi bi-file-earmark-text-fill fs-6"></i>
-                          </button>
-                          <button type="button" class="btn btn-pops">
-                            <i class="bi bi-trash fs-6"></i>
-                          </button>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="text-left pl-4">
-                          <span className="text-muted font-sm">
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing
-                            elit. Aenean
-                          </span>
-                        </td>
-                        <td className="text-left">
-                          <p className="text-muted font-sm word-limit">
-                            commodo ligula eget dolor. Aenean commodo ligula
-                            eget dolor. Aenean commodo ligula eget dolor. Aenean
-                            massa. Cum sociis natcommodo ligula eget dolor.
-                            Aenean massa. Cum sociis natcommodo ligula eget
-                            dolor. Aenean massa. Cum sociis natcommodo ligula
-                            eget dolor. Aenean massa. Cum sociis natoque
-                            penatibus et magnis dis parturient montes, nascetur
-                            ridiculus mus. Donec quam felis, ultricies nec,
-                            pellentesque eu, pretium quis, sem. Nulla consequat
-                            massa quis enim. Donec pede justo, fringilla vel,
-                            aliquet nec, vulputate eget, arcu.{" "}
-                            <span class="more-content"> .......</span>
-                          </p>
-                        </td>
-                        <td className="text-center">
-                          <span class="label-status label-active">Active</span>
-                        </td>
-                        <td className="text-center">
-                          <button type="button" class="btn btn-pops">
-                            <i class="bi bi-file-earmark-text-fill fs-6"></i>
-                          </button>
-                          <button type="button" class="btn btn-pops">
-                            <i class="bi bi-trash fs-6"></i>
-                          </button>
-                        </td>
-                      </tr>
+                      {faqs.map((faq, index) => (
+                        <tr key={index}>
+                          <td className="text-left pl-4">
+                            <span className="text-muted font-sm">
+                              {faq.title}
+                            </span>
+                          </td>
+                          <td className="text-left">
+                            <p className="text-muted font-sm word-limit">
+                              {faq.description}
+                            </p>
+                          </td>
+
+                          <td className="text-left">
+                            <span
+                              className={`label-status ${
+                                faq.active ? "label-active" : "label-inactive"
+                              }`}
+                            >
+                              {faq.active ? "Active" : "Inactive"}
+                            </span>
+                          </td>
+                          <td className="text-left">
+      {faq.active ? (
+        <>
+          <button type="button" className="btn btn-pops">
+            <i className="bi bi-file-earmark-text-fill fs-6"></i>
+          </button>
+          <button type="button" className="btn btn-pops"
+          onClick={() => unpublishFaq(faq.id)}>
+            <i className="bi bi-trash fs-6"></i>
+          </button>
+        </>
+      ) : (
+        <button
+          type="button"
+          className="btn btn-pops "
+          onClick={() => publishFaq(faq.id, faq.active)}
+        >
+          <i className="bi bi-check-circle-fill fs-6"></i>
+        </button>
+      )}
+    </td>
+                        </tr>
+                      ))}
                     </tbody>
                   </table>
                 </div>
                 <div class="paginations text-center">
-            <ul class="pager">
-              <li><a class="pager-prev" href="#"></a> <i class="bi bi-caret-left-fill"></i></li>
-              <li><a class="pager-number active" href="#">1</a></li>
-              <li><a class="pager-number" href="#">2</a></li>
-              <li><a class="pager-number" href="#">3</a></li>
-              <li><a class="pager-number" href="#">4</a></li>
-              <li><a class="pager-number" href="#">5</a></li>
-              <li><a class="pager-number " href="#">6</a></li>
-              <li><a class="pager-number" href="#">7</a></li>
-              <li><a class="pager-next" href="#"></a><i class="bi bi-caret-right-fill"></i></li>
-            </ul>
-          </div>
+                  <ul class="pager">
+                    <li>
+                      <a class="pager-prev" href="#"></a>{" "}
+                      <i class="bi bi-caret-left-fill"></i>
+                    </li>
+                    <li>
+                      <a class="pager-number active" href="#">
+                        1
+                      </a>
+                    </li>
+                    <li>
+                      <a class="pager-number" href="#">
+                        2
+                      </a>
+                    </li>
+                    <li>
+                      <a class="pager-number" href="#">
+                        3
+                      </a>
+                    </li>
+                    <li>
+                      <a class="pager-number" href="#">
+                        4
+                      </a>
+                    </li>
+                    <li>
+                      <a class="pager-number" href="#">
+                        5
+                      </a>
+                    </li>
+                    <li>
+                      <a class="pager-number " href="#">
+                        6
+                      </a>
+                    </li>
+                    <li>
+                      <a class="pager-number" href="#">
+                        7
+                      </a>
+                    </li>
+                    <li>
+                      <a class="pager-next" href="#"></a>
+                      <i class="bi bi-caret-right-fill"></i>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
@@ -141,4 +192,5 @@ function AdminTodayForecast() {
     </div>
   );
 }
-export default AdminTodayForecast;
+
+export default AdminFaqManage;
