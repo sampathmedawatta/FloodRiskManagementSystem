7be59b618fd5f717a703e828056c14249cddc4d8
BKHChathuranga <umatissera@gmail.com>
2024-05-03 15:45:05 +0530

    completed ask your query page

--- client/src/Components/RegUser/AskQueryPage.js
+++ client/src/Components/RegUser/AskQueryPage.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import QueryTable from "./QueryTable";
+import InquiriesService from "../../services/inquires.service";
 
 const AskQueryPage = () => {
+  const [queries, setQueries] = useState(null);
+
+  useEffect(() => {
+    const fetchAllQueries = async () => {
+      try {
+        //TODO: should implement in a way that can filter in registered userId of query list
+        const queryList = await InquiriesService.getAllInquiries();
+        const activeQueries = queryList.filter((query) => query.inquiryStatus !== "DISABLE")
+        if (activeQueries) {
+          setQueries(activeQueries);
+        }
+      } catch (error) {
+        console.error("Error while fetching query data", error);
+      }
+    };
+    fetchAllQueries();
+  },[]);
   return (
     <div className="box-content">
       <div className="row">
-        <QueryTable/>
+        <QueryTable queryData={queries}/>
       </div>
     </div>
   );
--- client/src/Components/RegUser/QueryTable.js
+++ client/src/Components/RegUser/QueryTable.js
@@ -1,51 +1,246 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getCurrentDateInfo } from "../Shared/Utils";
+import InquiriesService from "../../services/inquires.service";
 
-const QueryTable = () => {
+const QueryTable = ({ queryData }) => {
+  const [queries, setQueries] = useState(queryData);
+  const [queryTitle, setQueryTitle] = useState("");
+  const [queryDescription, setQueryDescription] = useState("");
+  const [errors, setErrors] = useState({
+    titleError: "",
+    descriptionError: "",
+  });
+  const { currentDate } = getCurrentDateInfo();
+
+  const handleAskQuery = async (e) => {
+    e.preventDefault();
+    try {
+      if (queryTitle !== "" && queryDescription !== "") {
+        const bodyData = {
+          messageTitle: queryTitle,
+          messageDescription: queryDescription,
+          messageDate: currentDate,
+          inquiryStatus: "PENDING",
+        };
+        const response = await InquiriesService.createInquiry(bodyData);
+        if (response) {
+          // Reset input fields and clear errors upon successful submission
+          setQueries([...queries, bodyData]);
+          setQueryTitle("");
+          setQueryDescription("");
+          setErrors({
+            titleError: "",
+            descriptionError: "",
+          });
+        }
+      } else {
+        setErrors({
+          titleError: queryTitle === "" ? "Title can not be empty" : "",
+          descriptionError:
+            queryDescription === "" ? "Description can not be empty" : "",
+        });
+      }
+    } catch (error) {
+      console.error("Error while sending inquiry", error);
+    }
+  };
+
+  useEffect(() => {
+    setQueries(queryData);
+  }, [queryData]);
+
   return (
-    <div className="section-box">
-      <div className="container">
-        <div className="panel-white">
-          <div className="panel-head">
-            <div className="row">
-              <div className="col-md-12">
-                <h6 className="text-left">
-                  <i className="bi bi-tsunami fs-5" />
-                </h6>
+    <>
+      <div className="section-box query-section-box">
+        <div className="container">
+          <div className="panel-white">
+            <div className="panel-head">
+              <div className="row align-items-center">
+                <div className="col-md-8">
+                  <h6 className="text-left ">
+                    <i className="bi bi-person-raised-hand " />
+                    Ask Your Query
+                  </h6>
+                </div>
+                <div className="col-md-4 text-center mt-1 mb-1">
+                  <button
+                    type="button"
+                    class="btn ask-query-button"
+                    data-bs-toggle="modal"
+                    data-bs-target="#askQueryModal"
+                  >
+                    Ask a query
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="panel-body">
-            <div className="table-responsive">
-              <table className="table no-wrap user-table mb-0">
-                <thead className="border-bottom thead-header">
-                  <tr>
-                    <th
-                     
-                      className="col-8 pl-4 text-start"
-                      
-                    >
-                      Question Asked
-                    </th>
-                    <th
-                     
-                      className="col-4 pl-4 text-start"
-                      
-                    >
-                      Status
-                    </th>
-                  </tr>
-                </thead>
-                {/* <AdminFaqList
-          faqs={faqs}
-          toggleEditModal={toggleEditModal}
-          handleFAQAction={handleFAQAction}
-        /> */}
-              </table>
+            <div className="panel-body">
+              <div className="table-responsive">
+                <table className="table no-wrap user-table mb-0">
+                  <thead className="border-bottom thead-header">
+                    <tr>
+                      <th className="col-8 pl-4 text-start">Question Asked</th>
+                      <th className="col-4 pl-4 text-start">Status</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {Boolean(queries) &&
+                      queries.map((queryItem) => (
+                        <tr className="tr-border" key={queryItem.id}>
+                          <td className="pl-4">
+                            <span className="text-muted font-sm">
+                              {queryItem.messageDescription}
+                            </span>
+                          </td>
+                          <td>
+                            {queryItem.inquiryStatus === "REPLY" && (
+                              <>
+                                <button
+                                  type="button"
+                                  class="btn query-button-replied"
+                                  data-bs-toggle="modal"
+                                  data-bs-target={`#staticBackdrop-${queryItem.id}`}
+                                >
+                                  Replied
+                                </button>
+                                <div
+                                  class="modal fade"
+                                  id={`staticBackdrop-${queryItem.id}`}
+                                  data-bs-backdrop="static"
+                                  data-bs-keyboard="false"
+                                  tabindex="-1"
+                                  aria-labelledby={`staticBackdropLabel-${queryItem.id}`}
+                                  aria-hidden="true"
+                                >
+                                  <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content">
+                                      <div class="modal-header">
+                                        <h4
+                                          class="modal-title fs-5"
+                                          id="staticBackdropLabel"
+                                        >
+                                          {queryItem.messageTitle}
+                                        </h4>
+                                        <button
+                                          type="button"
+                                          class="btn-close"
+                                          data-bs-dismiss="modal"
+                                          aria-label="Close"
+                                        ></button>
+                                      </div>
+                                      <div class="modal-body">
+                                        <p class="query-title">
+                                          Query you asked
+                                        </p>
+                                        <p>{queryItem.messageDescription}</p>
+                                        <hr />
+                                        <p class="query-title">Reply</p>
+                                        <p>{queryItem.replyDescription}</p>
+                                        <hr />
+                                      </div>
+                                      <div class="modal-footer pt-0">
+                                        <button
+                                          type="button"
+                                          class="btn btn-secondary"
+                                          data-bs-dismiss="modal"
+                                        >
+                                          Close
+                                        </button>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </>
+                            )}
+                            {queryItem.inquiryStatus === "PENDING" && (
+                              <button
+                                type="button"
+                                class="btn query-button-pending"
+                              >
+                                Pending
+                              </button>
+                            )}
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <div
+        class="modal fade"
+        id="askQueryModal"
+        tabindex="-1"
+        aria-labelledby="askQueryModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5">Ask a question</h1>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div class="mb-3">
+                  <label for="recipient-name" class="col-form-label">
+                    Title
+                  </label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    value={queryTitle}
+                    onChange={(e) => setQueryTitle(e.target.value)}
+                  />
+                  {errors.titleError && (
+                    <span className="text-danger">{errors.titleError}</span>
+                  )}
+                </div>
+                <div class="mb-3">
+                  <label for="message-text" class="col-form-label">
+                    Description:
+                  </label>
+                  <textarea
+                    class="form-control"
+                    value={queryDescription}
+                    onChange={(e) => setQueryDescription(e.target.value)}
+                  ></textarea>
+                  {errors.descriptionError && (
+                    <span className="text-danger">
+                      {errors.descriptionError}
+                    </span>
+                  )}
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                class="btn forecast-button"
+                onClick={(e) => handleAskQuery(e)}
+              >
+                Send query
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
