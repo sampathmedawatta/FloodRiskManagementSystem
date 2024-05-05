291c57fae1f424ff6003e893f71b5d48569db8c2
Sam Medawatta <sampath.medawatta@gmail.com>
2024-05-06 03:50:33 +1000

    flood forecast model for common location

--- forecastModel/server/ml_model/flood_predictior_api.py
+++ forecastModel/server/ml_model/flood_predictior_api.py
@@ -18,10 +18,14 @@ class model_input(BaseModel):
     Rainfall :  List[float]
 
 # loading the saved model
-loaded_model = pickle.load(open('flood_forecast_model.sav', 'rb'))
+loaded_model_CLK = pickle.load(open('flood_forecast_model_CLK.sav', 'rb'))
+loaded_model_CC = pickle.load(open('flood_forecast_model_CLK.sav', 'rb'))
+loaded_model_SK = pickle.load(open('flood_forecast_model_CLK.sav', 'rb'))
+loaded_model_ST = pickle.load(open('flood_forecast_model_CLK.sav', 'rb'))
+loaded_model_YMT = pickle.load(open('flood_forecast_model_CLK.sav', 'rb'))
 
 @app.post('/flood_prediction')
-async def flood_predd(input_parameters : model_input):
+async def flood_predd(input_parameters : model_input, location : str):
     
     input_data = input_parameters.model_dump_json()
     input_dictionary = json.loads(input_data)
@@ -30,7 +34,18 @@ async def flood_predd(input_parameters : model_input):
        # new_data = pd.DataFrame(input_data.dict())
         new_data = pd.DataFrame(input_dictionary)
 
-        predicted_rainfall = loaded_model.predict(new_data)
+        if(location =='CLK'): #Cheung Chau
+            predicted_rainfall = loaded_model_CLK.predict(new_data)
+        elif(location =='CC'): # Chek Lap Kok
+            predicted_rainfall = loaded_model_CC.predict(new_data)
+        elif(location =='SK'): # Shek Kong
+            predicted_rainfall = loaded_model_SK.predict(new_data)
+        elif(location =='ST'): # Sha Tin
+            predicted_rainfall = loaded_model_ST.predict(new_data)
+        elif(location =='YMT'): # Yau Ma Tei
+            predicted_rainfall = loaded_model_YMT.predict(new_data)
+        else:
+            predicted_rainfall = loaded_model_CLK.predict(new_data)   
 
         # Convert predicted_rainfall to list
         predicted_rainfall_list = predicted_rainfall.tolist()
