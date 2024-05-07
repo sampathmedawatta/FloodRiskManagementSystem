9d59b19dddd50dee266b40a58137651dd293fd49
“Chankami” <chankami91@gmail.com>
2024-05-07 18:08:26 +1000

    adding preferred language

--- server/modules/userModel.js
+++ server/modules/userModel.js
@@ -47,6 +47,10 @@ const userSchema = mongoose.Schema(
       type: String,
       require: true,
     },
+    lang: {
+      type: String,
+      require: true,
+    },
     active: {
       type: Boolean,
       require: true,
