9203258990f2bf3d6a25e5c1eb233e2a44cdde1c
“Chankami91” <chankami91@gmail.com>
2024-05-09 13:50:28 +1000

    fix corss policy issue

--- forecastModel/server/main.py
+++ forecastModel/server/main.py
@@ -6,17 +6,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:8001",
-    "http://localhost:8000",
-    "http://127.0.0.1:8001",
-    "http://127.0.0.1:8000",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins='*',
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
