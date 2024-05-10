2bfd218f9a039165dd6ca756103ae58ba030ae67
“Chankami91” <chankami91@gmail.com>
2024-05-10 14:47:11 +1000

    alginment

--- client/src/Components/NonRegUser/NonRegDashbord.js
+++ client/src/Components/NonRegUser/NonRegDashbord.js
@@ -7,27 +7,21 @@ import EmergencyContactCard from "./EmergencyContactCard";
 function NonRegDashbord() {
   return (
     <div className="box-content">
-    <div className="row">
-    <div className="col-md-6">
+      <div className="row">
+        <div className="col-md-6">
           <ForecastCardHolder />
         </div>
 
-
-    <div className="col-md-6">
-      <div>
-      
-      <EmergencyContactCard />
-  
+        <div className="col-md-6">
+          <div>
+            <EmergencyContactCard />
+          </div>
+          <div>
+            <br></br>
+            <Map></Map>
+          </div>
+        </div>
       </div>
-      <div>
-        <br></br>
-      <Map></Map>
-      </div>
-         
-      </div>
-
-
-    </div>
     </div>
   );
 }
